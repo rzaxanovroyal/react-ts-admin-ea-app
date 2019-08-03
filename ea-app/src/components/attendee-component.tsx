fdf75fb78f1ace898b68f46f936ece306a67384e
Maxim Zviagintsev <maxim.zviagintsev@gmail.com>
2019-08-03 18:42:40 +0300

    Complete attendee table fill in logic

--- ea-app/src/components/attendee-component.tsx
+++ ea-app/src/components/attendee-component.tsx
@@ -5,23 +5,42 @@ import {fetchPassword, fetchUsername, prodURL} from "../shared/keys";
 import axios from "axios";
 import {DataState} from "../store/data/reducer";
 import {setAttendees} from "../store/data/actions";
+import {Table} from 'antd';
+import {ColumnProps} from 'antd/lib/table';
 
 interface OwnProps {
-    setAttendees(attendees: []): void
+    setAttendees(attendees: object): void
 }
 
 const mapStateToProps = ({data}: RootState): { data: DataState } => ({data});
 
 type Props = OwnProps & ReturnType<typeof mapStateToProps>;
 
-type State = Readonly<{}>;
+interface Attendee {
+    key: number,
+    firstName: string,
+    lastName: string,
+    email: string,
+    attendeeTags: string[]
+}
+
+type State = Readonly<{
+    attendeeData: Attendee[]
+}>;
 
 class AttendeeComponent extends PureComponent<Props, State> {
-    readonly state: State = {};
+    readonly state: State = {
+        attendeeData: [{
+            key: 0,
+            firstName: '',
+            lastName: '',
+            email: '',
+            attendeeTags: [''],
+        }]
+    };
 
     private fetchAttendees = (): void => {
-        const fetchURL = `${prodURL}/jsonapi/node/attendee/?filter[field_event_reference.field_event_access_code][value]=${this.props.data.eventCode}&fields[user--user]=name,mail&include=field_attendee_tags.vid&fields[node--attendee]=title,field_full_name,field_attendee_tags&fields[taxonomy_term--attendee_tags]=name`;
-
+        const fetchURL = `${prodURL}/jsonapi/node/attendee/?filter[field_event_reference.field_event_access_code][value]=${this.props.data.eventCode}&fields[user--user]=name,mail&include=field_attendee_tags.vid&fields[node--attendee]=title,field_first_name,field_last_name,field_attendee_tags&fields[taxonomy_term--attendee_tags]=name`;
         axios({
             method: 'get',
             url: `${fetchURL}`,
@@ -35,7 +54,7 @@ class AttendeeComponent extends PureComponent<Props, State> {
             }
         })
             .then(res => {
-                const attendees: [] = res.data.data;
+                const attendees: object = res.data;
                 this.props.setAttendees(attendees);
             })
             .catch(error => console.log(error));
@@ -45,9 +64,80 @@ class AttendeeComponent extends PureComponent<Props, State> {
         this.fetchAttendees();
     }
 
+    componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>, snapshot?: any): void {
+        const attendees: any = this.props.data.attendees;
+
+        if (attendees !== prevProps.data.attendees) {
+            const attendeeNames = attendees.data;
+            const attendeeTags: any = attendees.included;
+
+            const attendeeData = attendeeNames.map((attendeeName: any, index: number) => {
+                let tags: string[] = [];
+
+                const existingTagIDs: string[] = attendeeName.relationships.field_attendee_tags.data.map((tagID: any) => {
+                    return tagID.id
+                });
+
+                attendeeTags.map((attendeeTag: any) => {
+                    return (
+                        existingTagIDs.map((existingTagID: string) => {
+                                if (attendeeTag.id === existingTagID) {
+                                    return (
+                                        tags.push(attendeeTag.attributes.name)
+                                    )
+                                } else {
+                                    return null
+                                }
+                            }
+                        ))
+                });
+
+                const tagsString = tags.join(', ');
+
+                return {
+                    key: index,
+                    firstName: attendeeName.attributes.field_first_name,
+                    lastName: attendeeName.attributes.field_last_name,
+                    email: attendeeName.attributes.title,
+                    attendeeTags: tagsString
+                }
+            });
+            this.setState({
+                attendeeData: attendeeData
+            })
+        }
+    }
+
     render() {
+        const columns: ColumnProps<Attendee>[] = [
+            {
+                title: 'Edit',
+                key: 'edit',
+                render: () => <div>Edit</div>
+            },
+            {
+                title: 'First name',
+                dataIndex: 'firstName',
+                key: 'firstName',
+            },
+            {
+                title: 'Last name',
+                dataIndex: 'lastName',
+                key: 'lastName',
+            },
+            {
+                title: 'Email',
+                dataIndex: 'email',
+                key: 'email',
+            }, {
+                title: 'Attendee tags',
+                dataIndex: 'attendeeTags',
+                key: 'attendeeTags'
+            }
+        ];
+
         return (
-            <div>Attendee</div>
+            <Table<Attendee> dataSource={this.state.attendeeData} columns={columns}/>
         );
     }
 }
